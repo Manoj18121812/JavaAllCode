d8cf8071da3c461c499040bf0edec110e3f2a460
Manoj18121812 <gavhaneam@gmail.com>
2025-09-10 08:27:28 +0530

    scanne code

---
+++ InputUsingScannerInJava4.java
@@ -0,0 +1,15 @@
+package AllDailyPractice;
+
+import java.util.Scanner;
+
+public class InputUsingScannerInJava4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The Name");
+		String m=sc.next();
+		System.out.println("Your Name Is : "+m);
+
+	}
+
+}
