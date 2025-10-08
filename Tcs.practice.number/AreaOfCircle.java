fd4f778390fb58d0a0c7ff518ac02661aeb614f7
Manoj18121812 <gavhaneam@gmail.com>
2025-10-08 18:27:48 +0530

    daily code

---
+++ Tcs.practice.number/AreaOfCircle.java
@@ -0,0 +1,20 @@
+package tcs.practice.numbers;
+
+public class AreaOfCircle {
+
+	public static void main(String[] args) {
+		
+		int length = 5;
+
+		areaofcircle(length);
+	}
+
+	private static void areaofcircle(int length) {
+		
+		double area = 3.14 *length * length;
+		
+		System.out.println(area);
+		
+	}
+
+}
